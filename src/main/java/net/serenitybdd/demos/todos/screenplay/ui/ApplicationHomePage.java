b2c8ab1b8f066639bd083df75f15d95d6c865969
Ngộ Thiện Phương <ngothienphuong@gmail.com>
2019-01-25 11:25:03 +0700

    Update ApplicationHomePage.java

--- src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
+++ src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
@@ -3,5 +3,23 @@ package net.serenitybdd.demos.todos.screenplay.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://todomvc.com/examples/angularjs/#/")
-public class ApplicationHomePage extends PageObject {}
+@DefaultUrl("https://tfl.gov.uk")
+public class ApplicationHomePage extends PageObject {
+  
+    /*
+    public final static String PLAN_JOURNEY = ".plan-journey";
+
+    public void viewProfile() {
+        $(PLAN_JOURNEY).click();
+    }*/
+  
+  
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".plan-journey");
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".status-update");
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".maps");
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".fares-and-payments");
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".help-and-contact");
+    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".more");
+  
+  
+}
