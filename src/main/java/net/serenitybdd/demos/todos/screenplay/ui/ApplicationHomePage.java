0dbc22947249344a69d4eaba436cbc64d5952e7a
Jacob Vu <vuthelinh@gmail.com>
2019-01-25 11:00:23 +0700

    Change the DefaultUrl

--- src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
+++ src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
@@ -3,5 +3,5 @@ package net.serenitybdd.demos.todos.screenplay.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://todomvc.com/examples/angularjs/#/")
+@DefaultUrl("https://tfl.gov.uk")
 public class ApplicationHomePage extends PageObject {}
