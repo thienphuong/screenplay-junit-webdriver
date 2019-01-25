c8e4ef2674a2a5ddb423ea4ac1b7a822287856ea
Ngộ Thiện Phương <ngothienphuong@gmail.com>
2019-01-25 11:27:47 +0700

    Update ApplicationHomePage.java

--- src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
+++ src/main/java/net/serenitybdd/demos/todos/screenplay/ui/ApplicationHomePage.java
@@ -8,18 +8,17 @@ public class ApplicationHomePage extends PageObject {
   
     /*
     public final static String PLAN_JOURNEY = ".plan-journey";
-
     public void viewProfile() {
         $(PLAN_JOURNEY).click();
     }*/
   
   
     public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".plan-journey");
-    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".status-update");
-    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".maps");
-    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".fares-and-payments");
-    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".help-and-contact");
-    public final static Target PLAN_JOURNEY_TAB = Target.the("plan-journey navigation item").locatedBy(".more");
+    public final static Target STATUS_UPDATE_TAB = Target.the("status update navigation item").locatedBy(".status-update");
+    public final static Target MAPS_TAB = Target.the("maps navigation item").locatedBy(".maps");
+    public final static Target FARES_TAB = Target.the("fares navigation item").locatedBy(".fares-and-payments");
+    public final static Target HELP_TAB = Target.the("help & contacts navigation item").locatedBy(".help-and-contact");
+    public final static Target MORE_TAB = Target.the("more navigation item").locatedBy(".more");
   
   
 }
